283c8b05227d39bda52123d0f23e39827d5e05b4
SANGEETHA9S <sangeetha190925@gmail.com>
2025-04-12 16:07:09 +0530

    second

---
+++ js dom/main.js
@@ -0,0 +1,3 @@
+document.getElementById('joinBtn').addEventListener('click', function () {
+    console.log(document.getElementById('Firstname').value + document.getElementById('Lastname').value)
+})
